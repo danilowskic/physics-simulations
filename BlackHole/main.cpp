31bcaefdd3cc8bc0420668ee16bf4788601862ce
Cezary Danilowski <danilowski.cezary@gmail.com>
2025-08-27 17:43:11 +0200

    Added basic window print

---
+++ BlackHole/main.cpp
@@ -0,0 +1,32 @@
+//
+// Created by cezary on 8/27/25.
+//
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to init GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Black Hole Simulation", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
